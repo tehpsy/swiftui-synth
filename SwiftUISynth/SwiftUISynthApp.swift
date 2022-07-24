9e9c5bf609f984178cae84faa669842e419535b4
James Baxter <james.baxter@focusrite.com>
2022-07-24 14:35:35 +0800

    1. Initial commit

---
+++ SwiftUISynth/SwiftUISynthApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISynthApp.swift
+//  SwiftUISynth
+//
+//  Created by James Baxter on 24/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISynthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
